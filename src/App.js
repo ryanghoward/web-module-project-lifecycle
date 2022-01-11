217b33fa5fa0486bfc48108f0ddf3fcd0ce6c7f4
Ryan Howard <ryanghoward@protonmail.com>
2022-01-11 15:33:00 -0800

    User info rendering through props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,37 @@ import User from './components/User';
 import FollowerList from './components/FollowerList';
 
 class App extends React.Component {
+  state = {
+    currentUser: 'ryanghoward',
+    user: {
+      avatar_url: 'https://avatars.githubusercontent.com/u/72590723?v=4',
+      html_url: 'https://github.com/ryanghoward',
+      name: 'Ryan G. Howard',
+      login: 'ryanghoward',
+      location: 'Los Angeles, CA',
+      public_repos: '60',
+      followers: '35',
+      following: '39'
+    },
+    followers: [
+      {
+        login: 'PriscilaMonteiro',
+        avatar_url: 'https://avatars.githubusercontent.com/u/77358128?v=4',
+        html_url: 'https://github.com/PriscilaMonteiro'
+      },
+      {
+        login: 'PriscilaMonteiro',
+        avatar_url: 'https://avatars.githubusercontent.com/u/77358128?v=4',
+        html_url: 'https://github.com/PriscilaMonteiro'
+      },
+      {
+        login: 'PriscilaMonteiro',
+        avatar_url: 'https://avatars.githubusercontent.com/u/77358128?v=4',
+        html_url: 'https://github.com/PriscilaMonteiro'
+      }
+    ]
+  }
+
   render() {
     return(
       <div>
@@ -11,8 +42,8 @@ class App extends React.Component {
           <input placeholder={'Search GitHub...'} />
           <button>Search</button>
         </form>
-        <User />
-        <FollowerList />
+        <User user={this.state.user} />
+        <FollowerList followers={this.state.followers} />
       </div>
     );
   }
--- src/components/User.js
+++ src/components/User.js
@@ -2,11 +2,13 @@ import React from "react";
 
 class User extends React.Component {
   render() {
+    const { user } = this.props;
+
     return (
       <div id='userinfo'>
-        <img src='https://avatars.githubusercontent.com/u/72590723?v=4' />
-        <a href='https://github.com/ryanghoward' target='_blank'><h3>Ryan G. Howard</h3></a>
-        <p>@ryanghoward</p>
+        <img src={user.avatar_url} />
+        <a href={user.html_url} target='_blank'><h3>{user.name}</h3></a>
+        <p>@{user.login}</p>
         <p>Repos: 60</p>
         <p>Followers: 35</p>
         <p>Following: 39</p>
