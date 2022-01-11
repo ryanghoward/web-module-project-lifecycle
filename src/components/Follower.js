07ed5e9631091b8c601e9626cbb7ccb5d73f390e
Ryan Howard <ryanghoward@protonmail.com>
2022-01-11 15:38:35 -0800

    Follower component built and functional

---
+++ src/components/Follower.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Follower extends React.Component {
+  render() {
+    return (
+      <div className="follower">
+        <img width='200px' src='https://avatars.githubusercontent.com/u/77358128?v=4' />
+        <a href='https://github.com/PriscilaMonteiro' target='_black'><p>PriscilaMonteiro</p></a>
+      </div>
+    )
+  }
+}
+
+export default Follower;
--- src/components/FollowerList.js
+++ src/components/FollowerList.js
@@ -1,25 +1,13 @@
 import React from "react";
+import Follower from "./Follower";
 
 class FollowerList extends React.Component {
   render() {
     return (
-      <div id="followers">
-        <div className="follower">
-          <img width='200px' src='https://avatars.githubusercontent.com/u/77358128?v=4' />
-          <a href='https://github.com/PriscilaMonteiro' target='_black'><p>PriscilaMonteiro</p></a>
-        </div>
-        <div className="follower">
-          <img width='200px' src='https://avatars.githubusercontent.com/u/77358128?v=4' />
-          <a href='https://github.com/PriscilaMonteiro' target='_black'><p>PriscilaMonteiro</p></a>
-        </div>
-        <div className="follower">
-          <img width='200px' src='https://avatars.githubusercontent.com/u/77358128?v=4' />
-          <a href='https://github.com/PriscilaMonteiro' target='_black'><p>PriscilaMonteiro</p></a>
-        </div>
-        <div className="follower">
-          <img width='200px' src='https://avatars.githubusercontent.com/u/77358128?v=4' />
-          <a href='https://github.com/PriscilaMonteiro' target='_black'><p>PriscilaMonteiro</p></a>
-        </div>
+      <div className="followers">
+        <Follower />
+        <Follower />
+        <Follower />
       </div>
     )
   }
