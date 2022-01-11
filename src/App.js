b306510b468638ff8c20489e7a859aa6fae2e4e0
Ryan Howard <ryanghoward@protonmail.com>
2022-01-11 14:47:11 -0800

    Basic DOM built

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return(
+      <div>
+        <h1>GitHub Info</h1>
+        <form>
+          <input placeholder={'Search GitHub...'} />
+          <button>Search</button>
+        </form>
+        <img src='https://avatars.githubusercontent.com/u/72590723?v=4' />
+        <a href='https://github.com/ryanghoward' target='_blank'><h3>Ryan G. Howard</h3></a>
+        <p>@ryanghoward</p>
+        <p>Repos: 60</p>
+        <p>Followers: 35</p>
+        <p>Following: 38</p>
+      </div>
+    );
   }
 }
 
